c7cd45a6948669b85a3cc25764d391d46d06a5ba
Your Name <you@example.com>
2022-12-15 09:15:45 +0000

    ..

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="",
     long_description_content_type="text/plain",
     url="",
-    packages=setuptools.find_packages(),
+    packages=["rough_notation"],
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.6",
