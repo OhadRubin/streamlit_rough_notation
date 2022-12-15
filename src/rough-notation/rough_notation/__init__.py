43d29f34abb9f6f81f41f36ed5b708b6e54e49a2
Your Name <you@example.com>
2022-12-15 08:55:54 +0000

    ..

--- src/rough-notation/rough_notation/__init__.py
+++ src/rough-notation/rough_notation/__init__.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as components
 # the component, and True when we're ready to package and distribute it.
 # (This is, of course, optional - there are innumerable ways to manage your
 # release process.)
-_RELEASE = False
+_RELEASE = True
 
 # Declare a Streamlit component. `declare_component` returns a function
 # that is used to create instances of the component. We're naming this
@@ -141,61 +141,61 @@ def rough_notation(name, key=None):
 #     st.markdown(text)
 #     st.markdown("---")
 
-import streamlit as st
-import streamlit.components.v1 as components
-# pip install rough-notation==0.0.1
-from rough_notation import rough_notation
-import streamlit as st
-from datasets import load_dataset
-import numpy as np
+# import streamlit as st
+# import streamlit.components.v1 as components
+# # pip install rough-notation==0.0.1
+# from rough_notation import rough_notation
+# import streamlit as st
+# from datasets import load_dataset
+# import numpy as np
 
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
-with st.sidebar:
-  split = st.radio("Pick a split", ["train", "validation", "test"])
-@st.experimental_memo
-def get_data(split):
-  dataset = load_dataset("iohadrubin/wikitext_retro_all")[split]
-  dataset.set_format("numpy")
-  return dataset
-dataset = get_data(split)
-from transformers import AutoTokenizer
+# with st.sidebar:
+#   split = st.radio("Pick a split", ["train", "validation", "test"])
+# @st.experimental_memo
+# def get_data(split):
+#   dataset = load_dataset("iohadrubin/wikitext_retro_all")[split]
+#   dataset.set_format("numpy")
+#   return dataset
+# dataset = get_data(split)
+# from transformers import AutoTokenizer
 
-@st.experimental_memo
-def get_tokenizer():
-  return AutoTokenizer.from_pretrained("t5-base") 
+# @st.experimental_memo
+# def get_tokenizer():
+#   return AutoTokenizer.from_pretrained("t5-base") 
 
-tokenizer = get_tokenizer()
+# tokenizer = get_tokenizer()
 
-@st.experimental_memo
-def get_instance(split,index):
-  dataset = get_data(split)
-  input_ids = dataset[index]['input_ids']
-  nei_input_ids = dataset[index]['nei_input_ids']
-  input_ids = input_ids.reshape(-1,64)
+# @st.experimental_memo
+# def get_instance(split,index):
+#   dataset = get_data(split)
+#   input_ids = dataset[index]['input_ids']
+#   nei_input_ids = dataset[index]['nei_input_ids']
+#   input_ids = input_ids.reshape(-1,64)
   
-  input_text = tokenizer.batch_decode(input_ids)
+#   input_text = tokenizer.batch_decode(input_ids)
   
-  return input_text,nei_input_ids
-with st.sidebar:
-  index = st.number_input("Pick an index", min_value=0, max_value=len(dataset)-1, value=0)
-input_text,_ = get_instance(split,index)
-col1, col2 = st.columns([1, 2])
-with col2:
-  current_chunk = rough_notation(input_text, key="foo")
+#   return input_text,nei_input_ids
+# with st.sidebar:
+#   index = st.number_input("Pick an index", min_value=0, max_value=len(dataset)-1, value=0)
+# input_text,_ = get_instance(split,index)
+# col1, col2 = st.columns([1, 2])
+# with col2:
+#   current_chunk = rough_notation(input_text, key="foo")
 
-@st.experimental_memo
-def get_nei_text(split, index, current_chunk):
-  tokenizer = get_tokenizer()
-  _,nei_input_ids = get_instance(split,index)
-  nei_input_ids = nei_input_ids.reshape(-1,3,2*64)
-  nei_text = tokenizer.batch_decode(nei_input_ids[current_chunk])
+# @st.experimental_memo
+# def get_nei_text(split, index, current_chunk):
+#   tokenizer = get_tokenizer()
+#   _,nei_input_ids = get_instance(split,index)
+#   nei_input_ids = nei_input_ids.reshape(-1,3,2*64)
+#   nei_text = tokenizer.batch_decode(nei_input_ids[current_chunk])
   
-  return nei_text
+#   return nei_text
 
-with col1:
-  st.markdown("## Neighbors")
-  for i,text in enumerate(get_nei_text(split, index, current_chunk)):
-    st.markdown(f"### Neighbor {i+1}")
-    st.markdown(text)
-    st.markdown("---")
+# with col1:
+#   st.markdown("## Neighbors")
+#   for i,text in enumerate(get_nei_text(split, index, current_chunk)):
+#     st.markdown(f"### Neighbor {i+1}")
+#     st.markdown(text)
+#     st.markdown("---")
