155dc4cfd25f88694f3dbf5c947d726dd7b907f1
Your Name <you@example.com>
2022-12-15 12:33:21 +0000

    ..

--- rough_notation/__init__.py
+++ rough_notation/__init__.py
@@ -43,7 +43,7 @@ else:
 # `declare_component` and call it done. The wrapper allows us to customize
 # our component's API: we can pre-process its input args, post-process its
 # output value, and add a docstring for users.
-def rough_notation(name, key=None):
+def rough_notation(name, default=0,key=None):
     """Create a new instance of "rough_notation".
 
     Parameters
@@ -70,7 +70,8 @@ def rough_notation(name, key=None):
     #
     # "default" is a special argument that specifies the initial return
     # value of the component before the user has interacted with it.
-    component_value = _component_func(name=name, key=key, default=0)
+    assert isinstance(default, int)
+    component_value = _component_func(name=name, key=key, default=default)
 
     # We could modify the value returned from the component if we wanted.
     # There's no need to do this in our simple example - but it's an option.
