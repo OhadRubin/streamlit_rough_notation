7fc0eead4deca30638faa61ead54e8ba0f6c851c
Your Name <you@example.com>
2022-12-15 09:10:33 +0000

    added build

---
+++ rough-notation/rough_notation/frontend/build/precache-manifest.e6184683b92a42b146d6c53578ce755c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1e22fc328fc94f2fccd",
+    "url": "./static/js/main.a1e22fc3.chunk.js"
+  },
+  {
+    "revision": "272a9ab280bdc67f9a11",
+    "url": "./static/js/1.272a9ab2.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "14429c358e11429f9938605f474b72e3",
+    "url": "./index.html"
+  }
+];
