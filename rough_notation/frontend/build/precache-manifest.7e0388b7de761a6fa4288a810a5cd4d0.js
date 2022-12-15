450e886fdccc3ed75fb1806810944c5ffe8c06d5
Your Name <you@example.com>
2022-12-15 18:10:21 +0000

    ...

---
+++ rough_notation/frontend/build/precache-manifest.7e0388b7de761a6fa4288a810a5cd4d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bf1e004b387ac1540341185de57e7c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09608ba8012b80aa6d01",
+    "url": "./static/js/2.94c4d486.chunk.js"
+  },
+  {
+    "revision": "f926d02ebfa104a852f6b07f116902c2",
+    "url": "./static/js/2.94c4d486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47879fd973141ee6a26a",
+    "url": "./static/js/main.26f68ba3.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
